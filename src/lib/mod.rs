643e9684e859efd90462a68a2a9ec6d38ed5bc2a
hankotanks <hank@modernatomic.com>
2022-10-21 10:09:39 -0600

    Split the project in a library and a binary

---
+++ src/lib/mod.rs
@@ -0,0 +1,112 @@
+mod state;
+pub mod mesh;
+mod camera;
+mod light;
+mod vertex;
+mod depth_texture;
+
+use winit::{
+    event_loop,
+    window::WindowBuilder,
+    event,
+    event::WindowEvent,
+};
+
+pub struct SceneConfig {
+    pub frame_speed: f32
+}
+
+pub async fn run<F: 'static, G: 'static>(config: SceneConfig, mut mesh_initialize: F, mut mesh_update: G) 
+    where F: FnMut(&mut mesh::Mesh), G: FnMut(&mut mesh::Mesh) {
+
+    // Contains all of the scene's geometry
+    let mut mesh = mesh::Mesh::default();
+
+    // Initialize the mesh
+    mesh_initialize(&mut mesh);
+
+    // TODO: Not sure if this should be a member of State or remain separate
+    let mut camera_controller = camera::CameraController::new(0.025, 0.6);
+
+    let event_loop = event_loop::EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    // Contains ALL of the engine's mutable state
+    let mut state = state::State::new(&window).await;
+
+    let frame_update_speed = config.frame_speed.recip() as i32;
+    let mut frame_update_count = 0;
+    
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            event::Event::RedrawRequested(w_id) if w_id == window.id() => {                
+                state.update(&mesh);
+
+                match state.render() {
+                    Ok(..) => {  },
+                    Err(wgpu::SurfaceError::Lost) => state.redraw(),
+                    Err(wgpu::SurfaceError::OutOfMemory) => {
+                        *control_flow = event_loop::ControlFlow::Exit 
+                    },
+                    Err(e) => eprintln!("{:?}", e)
+                }
+            },
+
+            // Redraw
+            event::Event::MainEventsCleared => { window.request_redraw(); },
+
+            // Handle close and resize events
+            event::Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => {
+                match event {
+                    // Handle close behavior
+                    WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
+                        input:
+                        event::KeyboardInput {
+                                state: event::ElementState::Pressed,
+                                virtual_keycode: Some(
+                                    event::VirtualKeyCode::Escape
+                                ),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = event_loop::ControlFlow::Exit,
+
+                    // When the window is resized from a single edge
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size)
+                    },
+
+                    // Scaled with a drag-click from the corner of the window
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size) 
+                    },
+
+                    // Unhandled behavior
+                    _ => {}
+                }
+            },
+
+            // Any mouse inputs are sent directly to the camera controller
+            event::Event::DeviceEvent { ref event, .. } => {
+                camera_controller.process_events(
+                    event, 
+                    &window, 
+                    &mut state.camera
+                );
+            }
+
+            // Unhandled events
+            _ => {  }
+        }
+
+        if frame_update_count == frame_update_speed {
+            mesh_update(&mut mesh);
+            frame_update_count = 0;
+        } else {
+            frame_update_count += 1;
+        }
+    });
+}
